e5e48e754d5b700b745c326a8027e62b5c3359e3
Shubham Vishwakarma <shubham.vishwakarma@toothsi.in>
2021-12-10 16:23:59 +0530

    Minor Tweaks

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2102,7 +2102,7 @@ class MaskRCNN():
         # Conditional import to support versions of Keras before 2.2
         # TODO: remove in about 6 months (end of 2018)
         try:
-            from keras.engine import saving
+            from tensorflow.python.keras.saving import hdf5_format
         except ImportError:
             # Keras before 2.2 used the 'topology' namespace.
             from keras.engine import topology as saving
@@ -2126,10 +2126,12 @@ class MaskRCNN():
         if exclude:
             layers = filter(lambda l: l.name not in exclude, layers)
 
-        if by_name:
-            saving.load_weights_from_hdf5_group_by_name(f, layers)
-        else:
-            saving.load_weights_from_hdf5_group(f, layers)
+        # if by_name:
+        #     hdf5_format.load_weights_from_hdf5_group_by_name(f, layers)
+        # else:
+        #     hdf5_format.load_weights_from_hdf5_group(f, layers)
+        # print("Hi")
+        keras_model.load_weights(filepath, by_name=by_name)
         if hasattr(f, 'close'):
             f.close()
 
